b334d7d62324c3b64f7d59f6b77877dc1928058f
charlootz <117485803+charlootz@users.noreply.github.com>
2024-11-13 12:11:01 -0500

    Initial Commit

---
+++ ColorDrag/ColorDragApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorDragApp.swift
+//  ColorDrag
+//
+//  Created by Joseph Albanese on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorDragApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
